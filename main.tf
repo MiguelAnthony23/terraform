378574082f30aa63c6d2c7a5f2b964c54b4824e8
Miguel A <miguelanthony.portillo@tajamar365.com>
2025-05-27 15:24:02 +0200

    Cambio de Main linea 30 ip Estatica y sku Standart

--- main.tf
+++ main.tf
@@ -31,7 +31,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "public-ip-apache"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static" # IP estática para que no cambie
+  sku                 = "Standart" # IP estática para que no cambie
 }
 
 resource "azurerm_network_interface" "nic" {
